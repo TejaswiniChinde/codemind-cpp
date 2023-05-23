2d25e0d4fe3633172c03d89d7fbe3839e56380fd
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2023-05-23 22:08:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int a[n];     int i,c1=0,c2=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {           c1=c1+a[i];         }         else         {             c2=c2+a[i];         }     }     printf("%d",abs(c1-c2)); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int i,c1=0,c2=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+          c1=c1+a[i];   
+        }
+        else
+        {
+            c2=c2+a[i];
+        }
+    }
+    printf("%d",abs(c1-c2));
+}
